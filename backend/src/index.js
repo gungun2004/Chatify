32a2f992c746ea100239aeb3374f5b028564869d
tanu0900 <your_email@example.com>
2025-09-09 18:28:04 +0530

    chatify project

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,8 @@ app.use("/api/messages" , messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  // Use named parameter for catch-all route
+  app.get("/:all(*)", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
