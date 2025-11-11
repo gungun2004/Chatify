dcc53d40a9e39d261e475c991f79f00057296c27
gungun2004 <veenuamardhingra16@gmail.com>
2025-11-11 11:54:37 +0530

    Add build folder for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chatify-1-f1x5.onrender.com/api" : "/api",
   withCredentials: true,
 });
