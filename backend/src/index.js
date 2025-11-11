0937a0110be0f9c2c848fbb214984c4e36caa243
gungun2004 <veenuamardhingra16@gmail.com>
2025-11-11 11:55:30 +0530

    Add build folder for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -19,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://chatifyat1.onrender.com",
     credentials: true,
   })
 );
